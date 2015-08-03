d5da90fc255bc317c9ca0fde4198903295f65255
Miguel Angel Ranz Serra <miguelangelranz@gmail.com>
2015-08-03 19:19:37 +0000

    Commit 021 - Edición de preguntas

--- quiz/app.js
+++ quiz/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
+var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
 
@@ -23,6 +24,7 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
