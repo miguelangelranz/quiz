0e7ef4a49b6fa3e1bc1d72b6feaa9915df878625
Miguel Angel Ranz Serra <miguelangelranz@gmail.com>
2015-08-18 18:17:17 +0000

    Commit 024 - Crear comentarios

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -2,7 +2,16 @@ var models = require('../models/models.js');
 
 // Autoload - si la ruta incluye :quizId, carga el objeto en la request
 exports.load = function(req, res, next, quizId) {
-   models.Quiz.findById(quizId).then(
+   models.Quiz.find(
+      {  
+         where: {
+            id: Number(quizId)
+         },
+         include: [{
+            model: models.Comment
+         }]
+      }
+   ).then(
       function(quiz) {
          if (quiz) {
             req.quiz = quiz;
--- quiz/models/models.js
+++ quiz/models/models.js
@@ -4,18 +4,29 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite
-var sequelize = new Sequelize(null, 
-                              null, 
-                              null, 
-                              {dialect: "sqlite", 
-                               storage: "quiz.sqlite"}
+var sequelize = new Sequelize(
+                        null, 
+                        null, 
+                        null, 
+                        {  dialect: "sqlite", 
+                           storage: "quiz.sqlite"
+                        }
                     );
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-// Exportar definición de tabla Quiz
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+// Relacion entre ambas tablas
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+// Exportar definición de las tablas Quiz y Comment
 exports.Quiz = Quiz;
+exports.Comment = Comment;
 
 // Arranque de la BBDD
 sequelize.sync().then(function() {
